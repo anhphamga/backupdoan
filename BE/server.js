3420e3538cbe8de723ee1150aeffcce3f89d7534
anhphamga <anhpntse161827@fpt.edu.vn>
2026-05-07 11:33:34 +0700

    Fix CORS for Vercel frontend

--- BE/server.js
+++ BE/server.js
@@ -14,7 +14,14 @@ const app = express();
 const server = http.createServer(app);
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: true, // reflect request origin
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
